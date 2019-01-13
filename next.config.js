2e67e2087de469bd6be47df8f6c88a7f7ba780f7
Woody Evins <woody.evins@gmail.com>
2019-01-13 16:39:33 -0600

    update next config to pass env vars to server and client

--- next.config.js
+++ next.config.js
@@ -1,2 +1,15 @@
-const withSass = require('@zeit/next-sass')
-module.exports = withSass();
+// https://recodes.co/next-js-dotenv/
+
+require('dotenv').config()
+
+const path = require('path')
+const withSass = require('@zeit/next-sass');
+
+module.exports = withSass({
+  publicRuntimeConfig: {
+    COGNITO_IDENTITY_POOL_ID: process.env.COGNITO_IDENTITY_POOL_ID,
+    COGNITO_USER_POOL_ID: process.env.COGNITO_USER_POOL_ID,
+    COGNITO_CLIENT_ID: process.env.COGNITO_CLIENT_ID,
+    COGNITO_REGION: process.env.COGNITO_REGION
+  }
+})
