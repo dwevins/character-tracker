93777e6451f38b71e9ed0b752b2d5ae876ee062d
Warryn Evins <W.Hatmaker@yahoo.com>
2019-04-02 06:10:54 -0500

    make cognito settings available for client and server

--- next.config.js
+++ next.config.js
@@ -2,14 +2,16 @@
 
 require('dotenv').config()
 
-const path = require('path')
 const withSass = require('@zeit/next-sass');
 
+const cognitoSettings = {
+  COGNITO_IDENTITY_POOL_ID: process.env.COGNITO_IDENTITY_POOL_ID,
+  COGNITO_USER_POOL_ID: process.env.COGNITO_USER_POOL_ID,
+  COGNITO_CLIENT_ID: process.env.COGNITO_CLIENT_ID,
+  COGNITO_REGION: process.env.COGNITO_REGION
+}
+
 module.exports = withSass({
-  publicRuntimeConfig: {
-    COGNITO_IDENTITY_POOL_ID: process.env.COGNITO_IDENTITY_POOL_ID,
-    COGNITO_USER_POOL_ID: process.env.COGNITO_USER_POOL_ID,
-    COGNITO_CLIENT_ID: process.env.COGNITO_CLIENT_ID,
-    COGNITO_REGION: process.env.COGNITO_REGION
-  }
+  serverRuntimeConfig: cognitoSettings,
+  publicRuntimeConfig: cognitoSettings
 })
