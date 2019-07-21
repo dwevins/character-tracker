a04ccc2cb89bf39778accacfba4ae4d1f1aa911e
Woody Evins <woody.evins@gmail.com>
2019-07-21 18:35:12 -0500

    remove templating from Character page file

--- pages/character.jsx
+++ pages/character.jsx
@@ -1,76 +1,8 @@
 import Page from '../layouts/main';
-import { Section } from '../elements';
+import { Character } from '../templates'
 
 export default () => (
     <Page>
-        <div className="page page--character">
-            <Section>
-                <h1>Character</h1>
-                <form className="form" action="" id="character-form">
-                    <div className="field-group field-group--alt form__child">
-                        <div className="field field--long">
-                            <input className="field__input" type="text" id="character-name" name="character-name"/>
-                            <label className="field__label" htmlFor="character-name">Name</label>
-                        </div>
-                        <div className="field field--short">
-                            <input className="field__input" type="text" id="character-level" name="character-level"/>
-                            <label className="field__label" htmlFor="character-level">Level</label>
-                        </div>
-                    </div>
-
-                    <div className="field-group field-group--alt form__child">
-                        <div className="field field--long">
-                            <input className="field__input" type="text" id="character-race" name="character-race"/>
-                            <label className="field__label" htmlFor="character-race">Race</label>
-                        </div>
-                        <div className="field field--short">
-                            <input className="field__input" type="text" id="character-alignment" name="character-alignment"/>
-                            <label className="field__label" htmlFor="character-alignment">Alignment</label>
-                        </div>
-                    </div>
-
-                    <div className="field-group form__child">
-                        <div className="field">
-                            <input className="field__input" type="text" id="character-class" name="character-class"/>
-                            <label className="field__label" htmlFor="character-class">Class</label>
-                        </div>
-                        <div className="field">
-                            <input className="field__input" type="text" id="character-deity" name="character-deity"/>
-                            <label className="field__label" htmlFor="character-deity">Deity</label>
-                        </div>
-                    </div>
-
-                    <div className="form__child field-group">
-                        <div className="field">
-                            <input className="field__input" type="text" id="character-height" name="character-height"/>
-                            <label className="field__label" htmlFor="character-height">Height</label>
-                        </div>
-                        <div className="field">
-                            <input className="field__input" type="text" id="character-weight" name="character-weight"/>
-                            <label className="field__label" htmlFor="character-weight">Weight</label>
-                        </div>
-                        <div className="field">
-                            <input className="field__input" type="text" id="character-size" name="character-size"/>
-                            <label className="field__label" htmlFor="character-size">Size</label>
-                        </div>
-                    </div>
-
-                    <div className="form__child field-group">
-                        <div className="field">
-                            <input className="field__input" type="text" id="character-hair" name="character-hair"/>
-                            <label className="field__label" htmlFor="character-hair">Hair</label>
-                        </div>
-                        <div className="field">
-                            <input className="field__input" type="text" id="character-eye" name="character-eye"/>
-                            <label className="field__label" htmlFor="character-eye">Eye</label>
-                        </div>
-                        <div className="field">
-                            <input className="field__input" type="text" id="character-gender" name="character-gender"/>
-                            <label className="field__label" htmlFor="character-gender">Gender</label>
-                        </div>
-                    </div>
-                </form>
-            </Section>
-        </div>
+        <Character />
     </Page>
 )
