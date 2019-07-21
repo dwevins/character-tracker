8e446f71f40920572049f004e0e2892be4d0216b
Woody Evins <woody.evins@gmail.com>
2019-07-21 16:59:07 -0500

    explicitly export head

--- components/head.jsx
+++ components/head.jsx
@@ -1,8 +1,8 @@
-import Head from 'next/head'
+import DocHead from 'next/head'
 
-export default () => (
-    <Head>
+export const Head = () => (
+    <DocHead>
         <title>Character Tracker</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
+    </DocHead>
 )
