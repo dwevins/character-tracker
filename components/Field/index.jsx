3cd5982ee14ff7a8bf86628bc6747ee97a7dce69
Woody Evins <woody.evins@gmail.com>
2019-07-21 18:41:18 -0500

    add field styles

--- components/Field/index.jsx
+++ components/Field/index.jsx
@@ -1,6 +1,29 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  text-align: center;
+  width: 100%;
+`
+
+const Input = styled.input`
+  background-color: inherit;
+  border: none;
+  border-bottom: 1px solid black;
+  text-align: center;
+  width: 100%;
+
+  &:focus { outline: none }
+`
+
+const Label = styled.label`
+  width: 100%;
+`
+
 export const Field = ({ id, label, type }) => (
-  <div className="field field--long">
-    <input className="field__input" type={type} id={id} name={id} />
-    <label className="field__label" htmlFor={id}>{label}</label>
-  </div>
+  <Wrapper>
+    <Input className="field__input" type={type} id={id} name={id} />
+    <Label className="field__label" htmlFor={id}>{label}</Label>
+  </Wrapper>
 )
