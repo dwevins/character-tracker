2133e684f3c5b5ad6f78ec92bc5ef5513fc23c14
Woody Evins <woody.evins@gmail.com>
2019-07-21 16:15:41 -0500

    remove link o external css

--- layouts/main.jsx
+++ layouts/main.jsx
@@ -1,8 +1,9 @@
 import Head from '../components/head';
 import Header from '../components/header';
 import Footer from '../components/footer';
+
 import { Fragment } from 'react';
-import '../styles/styles.scss';
+
 
 export default ({ children }) => (
     <Fragment>
