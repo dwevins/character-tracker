b8417ded86e90d257e3b8d3e1f9355332c499cf5
Woody Evins <woody.evins@gmail.com>
2019-07-21 19:26:41 -0500

    add page wrapper styles

--- layouts/main.jsx
+++ layouts/main.jsx
@@ -1,12 +1,19 @@
-import { Footer, Head, Header } from '../components';
-import { Fragment } from 'react';
+import { Fragment } from 'react'
+import styled from 'styled-components'
+import { Footer, Head, Header } from '../components'
 
+const Content = styled.div`
+  flex-grow: 1;
+  padding: 2rem 3rem;
+`
 
 export default ({ children }) => (
-    <Fragment>
-        <Head />
-        <Header />
-        { children }
-        <Footer />
-    </Fragment>
+  <Fragment>
+    <Head />
+    <Header />
+    <Content>
+      {children}
+    </Content>
+    <Footer />
+  </Fragment>
 )
